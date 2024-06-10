acf0244785fa92c7b272604b4232bef4b1d8d0d2
Adrysson Santos <adryssonlc@gmail.com>
2024-06-09 21:45:13 -0300

    feat: cria base da home e da página de envio do arquivo

--- src/components/ui/file-uploader.tsx
+++ src/components/ui/file-uploader.tsx
@@ -1,28 +1,37 @@
+import React, { useState } from "react";
+
 type FileUploaderProps = {
-  file: File;
+  file?: File;
 }
-const FileUploader = ({ file }: FileUploaderProps) => {
+const FileUploader = ({ file: initialFile }: FileUploaderProps) => {
+  const [file, setFile] = useState<File | null>(initialFile || null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      setFile(event.target.files[0]);
+    }
+  };
 
   return (
     <div className = "flex flex-col gap-6">
       <div>
         <label htmlFor="file" className="sr-only">
-          Choose a file
+          Escolha um arquivo
         </label>
-        <input id="file" type="file" accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/vnd.ms-excel,text/csv" />
+        <input id="file" type="file" accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/vnd.ms-excel,text/csv" onChange={handleFileChange} />
       </div>
       {file && (
         <section>
-          <p className="pb-6">File details:</p>
+          <p className="pb-6">Detalhes do arquivo:</p>
           <ul>
-            <li>Name: {file.name}</li>
-            <li>Type: {file.type}</li>
-            <li>Size: {file.size} bytes</li>
+            <li>Nome: {file.name}</li>
+            <li>Tipo: {file.type}</li>
+            <li>Tamanho: {file.size} bytes</li>
           </ul>
         </section>
       )}
 
-      {file && <button className="rounded-lg bg-green-800 text-white px-4 py-2 border-none font-semibold">Upload the file</button>}
+      {file && <button className="rounded-lg bg-green-800 text-white px-4 py-2 border-none font-semibold">Fazer upload do arquivo</button>}
     </div>
   );
 };
